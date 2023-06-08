0443d8f8f885809df85a083fa1412a9a9da978a1
DESKTOP-EIEQFRR\Ollie <owoo0002@student.monash.edu>
2023-06-09 00:55:55 +0800

    install tailwindcss via npm

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./templates/**/*.html",
+    "./static/src/**/*.js"
+  ],
+  theme: {
+    extend: {},
+    fontFamily: {
+      sans: ['Graphik', 'ui-sans-serif', 'system-ui'],
+      serif: ['ui-serif', 'Georgia'],
+      mono: ['ui-monospace', 'SFMono-Regular']
+    },
+  },
+  plugins: [],
+}
